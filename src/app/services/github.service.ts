bec524e24d6ebc28553abb45a71c3627dd28a45b
Saney Alam <codecamper@outlook.com>
2017-06-11 17:16:36 -0400

    Intial Commit

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -6,6 +6,8 @@ import 'rxjs/add/operator/map';
 export class GithubService {
 
   private userName:string;
+  private client_id ='a13cea87f4bf83ffb39b';
+  private client_secret='859f4812bfb10e98fd59f837640c6541417d34b5';
 
   constructor(private _http:Http) { 
 
@@ -15,12 +17,12 @@ export class GithubService {
   }
 
   getUser(){
-    return this._http.get('http://api.github.com/users/'+ this.userName).map(res => res.json());
+    return this._http.get('http://api.github.com/users/'+ this.userName + '?client_id='+this.client_id + '&client_secret='+ this.client_secret).map(res => res.json());
 
   }
 
   getRepo(){
-    return this._http.get('http://api.github.com/users/'+ this.userName + '/repos').map(res => res.json());
+    return this._http.get('http://api.github.com/users/'+ this.userName + '/repos?client_id='+ this.client_id + '&client_secret='+ this.client_secret).map(res => res.json());
 
   }
 
